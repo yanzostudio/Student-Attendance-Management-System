75cd65be94d8440b6a8e37cddf6b51f78acaf73c
Ichiro <ichiro051202@gmail.com>
2025-01-27 00:50:16 +0800

    test

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AI-Enhanced Student Attendance Management System</title>
+    <title>STUDENT ATTENDANCE MANAGEMENT SYSTEM</title>
     <link rel="stylesheet" href="css/login-page-styles.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
@@ -16,7 +16,7 @@
         <form action="login.php" method="POST">
    
             <div class="top">
-                <header>AI - Student Attendance <br> Management System</header>
+                <header>Student Attendance <br> Management System</header>
             </div>
             <div class="logo"></div>
 
